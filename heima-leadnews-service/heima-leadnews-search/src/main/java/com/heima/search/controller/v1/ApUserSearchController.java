e39445813358379f61f99c160aabdcf666cdbc6e
plume <birdgray@163.com>
2023-07-19 15:33:12 +0800

    删除历史搜索记录

--- heima-leadnews-service/heima-leadnews-search/src/main/java/com/heima/search/controller/v1/ApUserSearchController.java
+++ heima-leadnews-service/heima-leadnews-search/src/main/java/com/heima/search/controller/v1/ApUserSearchController.java
@@ -1,10 +1,12 @@
 package com.heima.search.controller.v1;
 
 import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.search.dtos.HistorySearchDto;
 import com.heima.search.service.ApUserSearchService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +28,11 @@ public class ApUserSearchController{
     @PostMapping("/load")
     public ResponseResult findUserSearch() {
         return apUserSearchService.findUserSearch();
+    }
+
+    @PostMapping("/del")
+    public ResponseResult delUserSearch(@RequestBody HistorySearchDto dto) {
+        return apUserSearchService.delUserSearch(dto);
     }
 
 }
