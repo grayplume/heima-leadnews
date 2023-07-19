2593680524399caaab31af516cefb70332d3f89c
plume <birdgray@163.com>
2023-07-19 15:19:37 +0800

    搜索历史保存十条进dongodb

--- heima-leadnews-service/heima-leadnews-search/src/main/java/com/heima/search/interceptor/AppTokenInterceptor.java
+++ heima-leadnews-service/heima-leadnews-search/src/main/java/com/heima/search/interceptor/AppTokenInterceptor.java
@@ -1,18 +1,16 @@
-package com.heima.wemedia.interceptor;
+package com.heima.search.interceptor;
 
-import com.heima.model.wemedia.pojos.WmUser;
+import com.heima.model.user.pojos.ApUser;
 import com.heima.utils.thread.AppThreadLocalUtil;
-import com.heima.utils.thread.WmThreadLocalUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Optional;
 
 @Slf4j
-public class WmTokenInterceptor implements HandlerInterceptor {
+public class AppTokenInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         //得到header中的信息
@@ -20,20 +18,15 @@ public class WmTokenInterceptor implements HandlerInterceptor {
         Optional<String> optional = Optional.ofNullable(userId);
         if(optional.isPresent()){
             //把用户id存入threadloacl中
-            WmUser wmUser = new WmUser();
-            wmUser.setId(Integer.valueOf(userId));
-            WmThreadLocalUtil.setUser(wmUser);
-            log.info("wmTokenFilter设置用户信息到threadlocal中...");
+            ApUser apUser = new ApUser();
+            apUser.setId(Integer.valueOf(userId));
+            AppThreadLocalUtil.setUser(apUser);
+            log.info("apTokenFilter设置用户信息到threadlocal中...");
         }
 
         return true;
     }
 
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        // log.info("清理threadlocal...");
-        // WmThreadLocalUtil.clear();
-    }
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         log.info("清理threadlocal...");
